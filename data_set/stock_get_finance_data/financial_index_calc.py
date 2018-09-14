9a259d0a5ff041bec50bc529b148dfa34df1e555
luozero <luozero@126.com>
2018-09-14 08:09:29 +0800

    add finance index calc file

--- data_set/stock_get_finance_data/financial_index_calc.py
+++ data_set/stock_get_finance_data/financial_index_calc.py
@@ -16,19 +16,36 @@ def load_financical_data(path, stock_code):
     csv_file_path = os.path.join(path, ite)
     if os.path.exists(csv_file_path):
       data = pd.read_csv(csv_file_path, encoding='ANSI')
+      data = data.replace('--', 0)
+      data = data.fillna(0)
     data_file.append(data)
   return data_file
+
+def div_calc(a,b):
+  data1 = a.values.squeeze()
+  data1 = np.float32(data1[1:])
+  data2 = b.values.squeeze()
+  data2 = np.float32(data2[1:])
+  data = data1/data2
+  data = data.dot(100)
+  return data
+
+def earning_quality_calc(data_main, data_profit):
+  equlity = data_main[data_main['报告日期'].isin(['股东权益不含少数股东权益(万元)'])]
+  earning = data_profit[data_profit['报告日期'].isin(['净利润(万元)'])]
+  cost = data_main[data_main['报告日期'].isin(['营业总成本(万元)'])]
+  roe = div_calc(earning,equlity)
+  asset = data_main[data_main['报告日期'].isin(['总资产(万元)'])]
+  roa = div_calc(earning,asset)
+  revenue = data_main[data_main['报告日期'].isin(['主营业务收入(万元)'])]
+  profit_revenue = div_calc(earning,revenue)
+  profit_cost = div_calc(earning,cost)
+  
+  return roe
+
 
 if __name__ == '__main__':
   stock_code = '000001'
   data = load_financical_data('../../../data/finance', stock_code)
   data_main = data[0]
-  equlity = data_main[data_main['报告日期'].isin(['股东权益不含少数股东权益(万元)'])]
-  equlity = equlity.fillna(0)
-  earning = data_main[data_main['报告日期'].isin(['净利润(万元)'])]
-  earning = earning.fillna(0)
-  data1 = earning.values.squeeze()
-  data1 = np.float32(data1[1:])
-  data2 = equlity.values.squeeze()
-  data2 = np.float32(data2[1:])
-  roe = data1/data2*100
+
