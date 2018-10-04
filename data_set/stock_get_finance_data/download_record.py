595e605433fccca1dced3bc6a1d2e8b4a120ffb3
luozero <luozero@126.com>
2018-10-04 13:23:21 +0800

    download stock basic

---
+++ data_set/stock_get_finance_data/download_record.py
@@ -0,0 +1,39 @@
+# coding: utf8
+import pandas as pd
+import os
+import json
+class download_record:
+  def __init__(self,path='../../../data/',record='rec.json', skip = 'skip_stock.csv'):
+    self.path_file = os.path.join(path,record)
+    self.path_skip_file = os.path.join(path,skip)
+    '''
+    classdocs
+    '''
+  def read_index(self):
+    if not os.path.exists(self.path_file):
+      return 0
+    with open(self.path_file) as f:
+        summary = json.load(f)
+    index = summary['index']
+    return index
+
+  def write_index(self, index):
+    with open(self.path_file, 'w') as f:
+      summary = dict(
+              index = index
+      )
+      json.dump(summary, f)
+
+  def write_skip_stock(self,stock):
+    if os.path.exists(self.path_skip_file):
+      pd_skip = pd.read_csv(self.path_skip_file)
+      pd_skip1 = pd.DataFrame([stock],columns=['stock'])
+      pd_skip = pd_skip.append(pd_skip1,ignore_index=True)
+    else:
+      pd_skip = pd.DataFrame([stock],columns=['stock'])
+    pd_skip.to_csv(self.path_skip_file, index = False)
+    
+  def read_skip_stock(self):
+    data = pd.read_csv(self.path_skip_file)
+    return data
+    
--- data_set/stock_get_finance_data/stock_basic.py
+++ data_set/stock_get_finance_data/stock_basic.py
@@ -10,29 +10,60 @@ import datetime
 import os
 import financial_download as FD
 from financial_load_store import financial_load_store as FLD
-
-MAX_DOWNLOAD_TIMES = 30
+from download_record import download_record as DR
+MAX_DOWNLOAD_TIMES = 50
 
 class stock_basic:
   def __init__(self, stock_codes=['000001'], path_root='../../../data/'):
     ts.set_token('857448afe8a838163de2ea7e3555468ca0f314a424ad47da078c9265')
+    self.pro = ts.pro_api()
     path = os.path.join(path_root,'stock_basic')
     if not os.path.exists(path):
               os.makedirs(path)
     self.path = path
     self.stock_codes = stock_codes
     self.FLD = FLD(path=path_root)
+    self.DR = DR(path_root,'stock_basic.json')
     
   def try_download_csv(self,ts_code,date):
     stock_basic = pd.DataFrame()
     try:
-      pro = ts.pro_api()
-      stock_basic = pro.daily_basic(ts_code=ts_code, trade_date=date)
+      stock_basic = self.pro.daily_basic(ts_code=ts_code, trade_date=date)
       continue_download_this_stock = False
     except :
       print('fail to download stock ',ts_code, 'date',date,'one times, and try it again!!!')
       continue_download_this_stock = True
     return continue_download_this_stock, stock_basic
+  
+  def get_all_stocks_basic(self): 
+    process_index = SB.DR.read_index()
+    print('process_index',process_index)
+    for stock in self.stock_codes[process_index:]:
+      print('stock', stock, 'is downloading','process_index',process_index)
+      try_cnt = 0
+      stock_store = stock
+      if int(stock)<600000:
+        stock = stock + '.SZ'
+      else:
+        stock = stock + '.SH'
+      while True:
+        try_cnt = try_cnt + 1
+        continue_download_this_stock = True
+        while continue_download_this_stock:
+          continue_download_this_stock,stock_basic = self.try_download_csv(stock, '')
+          print('stock', stock, 'try times', try_cnt)
+        if try_cnt == MAX_DOWNLOAD_TIMES:
+          print('skip this stock', stock, 'try times', try_cnt)
+          SB.DR.write_skip_stock(stock)
+          break
+        if not stock_basic.empty:
+          break
+      path_csv = os.path.join(self.path,stock_store+'_basic.csv')
+      stock_basic.to_csv(path_csv)
+      print('this stock',stock, 'successfully downloaded')
+      process_index = process_index + 1
+      SB.DR.write_index(process_index)
+    
   
   def get_daily_basic(self,ts_code,get_date,must_download_flag): 
     
@@ -97,12 +128,12 @@ class stock_basic:
     pd_skipstock = pd.Series(pd_skipstock)
     pd_skipstock.to_csv(path_csv)
     
-  
 if __name__ == '__main__':
   path_root = '../../../data/'
   #stock_codes = ['000001']
   stock_codes = FD.ts_stock_codes()
   #get_dates = ['2018/6/30']
   SB = stock_basic(stock_codes, path_root)
-  SB.get_stocks_basic()
+    #SB.get_stocks_basic()
+  SB.get_all_stocks_basic()
   pass
