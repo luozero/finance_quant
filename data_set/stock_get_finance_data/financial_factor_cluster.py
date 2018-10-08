9cbe2eb7cfb70415e2b0384625457f0c939689ff
luozero <luozero@126.com>
2018-10-08 19:40:58 +0800

    add hierarchical cluster

--- data_set/stock_get_finance_data/financial_factor_cluster.py
+++ data_set/stock_get_finance_data/financial_factor_cluster.py
@@ -8,51 +8,53 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import spectral_clustering
 from sklearn.cluster import AgglomerativeClustering
-from financial_index_calc import finance_index_dic as FID
-from financial_index_rank import financial_index_rank
+from financial_factor_calc import finance_index_dic as FID
+from financial_factor_io import financial_factor_io as FIO
+from stock_codes_utility import stock_codes_utility as SCU
 import financial_download
 import os
 import sys, getopt
 
 class financial_factor_cluster:
-  def __init__(self, path = '../../../data/finance_processed', path_score='../../../data/score', path_score_cluster='../../../data/score', 
-               stocks = ['000001'], dates=['2018-06-30'], indexs=['roe'],alg = 'kmean'):
-    self.path_score = path_score
+  def __init__(self, path = '../../../data/', path_factor = '../../../data/factor_io',
+               path_cluster='../../../data/factor_cluster',
+               stocks = ['000001'], dates=['2018-06-30'], indexs=['roe'],alg = 'kmean',file_name = '1806_1712_1'):
     self.stocks = stocks
     self.dates = dates
     self.indexs = indexs
-    FIR = financial_index_rank(path=path, path_score=path_score, stocks = stocks, dates = dates)
+    fio = FIO(path=path, path_factor=path_factor, stocks = stocks, dates = dates, file_name = file_name)
     
-    if not os.path.exists(path_score_cluster):
-      os.makedirs(path_score_cluster)
-    self.feth_indexs = FIR.fetch_selected_financial_indexs(indexs, dates,path_score_cluster)
-    self.path_index_score = os.path.join(path_score_cluster,'index_score_{}'.format(alg)+'_cluster_{}.csv')
+    if not os.path.exists(path_cluster):
+      os.makedirs(path_cluster)
+    self.fetch_factors = fio.fetch_selected_factors(indexs, dates)
+    self.path_cluster = os.path.join(path_cluster,'{}'.format(alg)+'_cluster_{}.csv')
     self.alg = alg
     
   def cluster_financial_indexs(self, k):
     #fecth_indexs = FIR.fetch_selected_financial_indexs(indexs, self.dates)
     #date = '2017-12-31'
-    for date in dates:
-      print('kmean date is', date)
-     # k = 10
-      if self.alg=='kmean':
-        km = KMeans(n_clusters=k, random_state=42)
-        km.fit(self.feth_indexs[date].values)
-        labels = km.labels_
-      else:
-        ward = AgglomerativeClustering(n_clusters=k, linkage='ward')
-        ward.fit(self.feth_indexs[date].values)
-        labels = ward.labels_
-        
-      #labels = spectral_clustering(self.feth_indexs[date].values, n_clusters=k,
-      # assign_labels='discretize', random_state=1)
+    date = dates[0]
+    print('cluster is', date)
+   # k = 10
+    if self.alg=='kmean':
+      km = KMeans(n_clusters=k, random_state=42)
+      km.fit(self.fetch_factors.values)
+      labels = km.labels_
+    else:
+      ward = AgglomerativeClustering(n_clusters=k, linkage='ward')
+      ward.fit(self.fetch_factors.values)
+      labels = ward.labels_
+      
+    #labels = spectral_clustering(self.fetch_factors[date].values, n_clusters=k,
+    # assign_labels='discretize', random_state=1)
 
-      self.feth_indexs[date]["Cluster"] = labels
-      #self.feth_indexs[date]["Cluster"].sort(key='Cluster', reverse=False)
-      self.feth_indexs[date] = self.feth_indexs[date].sort_values("Cluster",axis=0,ascending=True)
-      self.feth_indexs[date].to_csv(self.path_index_score.format(date))
+    self.fetch_factors["Cluster"] = labels
+    #self.fetch_factors[date]["Cluster"].sort(key='Cluster', reverse=False)
+    self.fetch_factors = self.fetch_factors.sort_values("Cluster",axis=0,ascending=True)
+    self.fetch_factors.to_csv(self.path_cluster.format(date))
+    print('save folder is', self.path_cluster.format(date))
 
-#python financial_factor_cluster.py -o ../../../data/score/k100 -k 100 -d 2017-12-31
+#python financial_factor_cluster.py -o ../../../data/score/k100 -k 100 -d 2017-12-31 -a Agglomerative
 if __name__ == '__main__':
   outputfile = ''
   try:
@@ -72,12 +74,13 @@ if __name__ == '__main__':
         date = arg
     elif opt in ("-a", "--algorithm"):
         alg = arg
-  path = '../../../data/finance_processed'
-  path_score = '../../../data/score'
-  path_score_cluster = outputfile
-  stocks = financial_download.ts_stock_codes()
+  path = '../../../data/'
+  path_factor = '../../../data/factor_io'
+  path_cluster = outputfile
+  scu = SCU(path=path)
+  stocks = scu.stock_codes_remove_no_stock_basic()
   #stocks = ['000001','000002','000004','000005','000006']
-  dates = [date]
+  dates = ['2018-06-30','2017-12-31']
   #k = 300
   #FIR = financial_index_rank(path=path, path_score=path_score, stocks = stocks, dates = dates)
   indexs = [
@@ -85,13 +88,13 @@ if __name__ == '__main__':
     FID['roe'],\
     FID['roa'],\
     FID['profit_revenue'],\
-    FID['profit_cost'],\
-    FID['equlity_incr_rate'],\
+    #FID['profit_cost'],\
+    #FID['equlity_incr_rate'],\
     ###grow capacity
-    FID['revenue_incr_rate'],\
-    FID['profit_incr_rate'],\
-    FID['cash_incr_rate'],\
-    FID['asset_incr_rate'],
+    #FID['revenue_incr_rate'],\
+    #FID['profit_incr_rate'],\
+    #FID['cash_incr_rate'],\
+    #FID['asset_incr_rate'],
   ]
   '''
   FID['profit_revenue'],\
@@ -103,8 +106,9 @@ if __name__ == '__main__':
   FID['cash_incr_rate'],\
   FID['asset_incr_rate'],
   '''
-  path_score_cluster = os.path.join(path_score_cluster)
-  FIKC = financial_factor_cluster(path=path, path_score=path_score, path_score_cluster=path_score_cluster, stocks=stocks,dates=dates,indexs=indexs,alg=alg)
-  FIKC.cluster_financial_indexs(k)
+  path_cluster = os.path.join(path_cluster)
+  FFC = financial_factor_cluster(path=path, path_factor=path_factor, path_cluster=path_cluster,\
+                                  stocks=stocks,dates=dates,indexs=indexs,alg=alg,file_name = alg)
+  FFC.cluster_financial_indexs(k)
   #fecth_indexs = FIR.fetch_selected_financial_indexs(indexs, dates)
   pass
--- data_set/stock_get_finance_data/financial_factor_io.py
+++ data_set/stock_get_finance_data/financial_factor_io.py
@@ -40,22 +40,32 @@ class financial_factor_io:
   
   def fetch_one_factor(self, factor, dates):
     factor_value = pd.DataFrame(dtype = float)
+    column = []
+    column_flag =True
     for stock_code in self.stock_codes:
       factor_temp = []
       for date in dates:
+        if column_flag==True:
+          column.append(factor+date)
         print('date',date,'stock_code',stock_code)
         factor_temp.append(self.financial_factors[stock_code].loc[date,factor])
+      column_flag = False
       factor_value = pd.concat([factor_value, pd.DataFrame([factor_temp])],axis=0)
       #factor_value = factor_value.append(factor_temp,index = stock_code)
-    return factor_value
+    return factor_value, column
   
   def fetch_selected_factors(self, factors, dates):
     if not os.path.exists(self.path_factor_io):
       pd_factor_values = pd.DataFrame(dtype=float)
+      list_columns = []
       for factor in factors:
         print('processing factor is', factor)
-        factor_value = self.fetch_one_factor(factor, dates)
+        factor_value, column = self.fetch_one_factor(factor, dates)
+        list_columns = list_columns + column
         pd_factor_values = pd.concat([pd_factor_values, factor_value], axis=1)
+      pd_factor_values.columns = list_columns
+      scu = SCU(path=self.path)
+      pd_factor_values.index = scu.add_allstock_sh_sz(self.stock_codes)
       pd_factor_values.to_csv(self.path_factor_io, encoding='ANSI')
     else:
       pd_factor_values = pd.read_csv(self.path_factor_io,index_col=0)
@@ -67,10 +77,11 @@ if __name__ == '__main__':
   path_factor = '../../../data/factor_io'
   print(FID['roe'])
   scu = SCU(path=path)
-  stock_codes = scu.stock_codes_remove_no_stock_basic()
+  stocks = scu.stock_codes_remove_no_stock_basic()
   stocks = ['000001','000002','000004','000005','000006']
   dates = ['2018-06-30','2017-12-31']#,'2017-12-31'
-  ffio = financial_factor_io(path=path, path_factor=path_factor, stocks = stocks, dates = dates)
+  ffio = financial_factor_io(path=path, path_factor=path_factor, \
+                             stocks = stocks, dates = dates,file_name = '1806_1712_1')
   indexs = [
     #earning capacity
     FID['roe'],\
