d7e6c5e2a07494f3b24f7dafa6c2d9bf68630449
luozero <luozero@126.com>
2020-02-10 09:42:10 +0800

    first AI momentum

--- training/data_loader.py
+++ training/data_loader.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import os
+import torch
 
 proc_days = 5
 
-class stock_loader:
-  def _init_(self, stock_code = "000001.XSHE", path="../../../data", t_vs_t = 0.1, train_day = "5day"):
+class StockLoader:
+  def __init__(self, stock_code = "000001.XSHE", path="../../../data", t_vs_t = 0.1, train_day = "5day"):
     factor_csv = os.path.join(path, stock_code + ".csv")
-    self.data = pd.read_csv(factor_csv)
+    self.data = pd.read_csv(factor_csv, index_col=0)
     self.train_day = train_day
     row, col = self.data.shape
     self.train_num = int(row * t_vs_t)
-    self.test_num = row - self.train_num - proc_days
+    self.test_num = row
     self.train_index = 1
     self.test_index = self.train_num + 1
     self.proc_num = row
@@ -18,16 +19,30 @@ class stock_loader:
   def train_loader(self):
     if self.train_index > self.train_num:
       return
-    train_output = self.data.ix[self.train_index:self.train_num, self.train_day]
-    train_input = self.data.ix[self.train_index:self.train_num, 1:-proc_days]
-    self.train_index += 1
-    return train_input, train_output
+    train_output = self.data.loc[:, self.train_day]
+    train_output =  train_output.iloc[self.train_index: self.train_num]
+    train_input = self.data.iloc[:, 0:-proc_days]
+    train_input = train_input.iloc[self.train_index: self.train_num, :]
+    train_data = [];
+    index = 0
+    for output in train_output:
+      input = train_input.iloc[index, :]
+      train_data.append(tuple([torch.Tensor(input.values), output]))
+      index +=1
+    return train_data
 
   def test_loader(self):
     if self.test_index > self.test_num:
       return
-    test_output = self.data.ix[self.test_index:self.test_num , self.train_day]
-    test_input = self.data.ix[self.test_index:self.test_num , 1:-proc_days]
-    self.test_index +=1
-    return test_input, test_output
+    test_output = self.data.loc[:, self.train_day]
+    test_output =  test_output.iloc[self.test_index: self.test_num]
+    test_input = self.data.iloc[:, 0:-proc_days]
+    test_input = test_input.iloc[self.test_index: self.test_num, :]
+    test_data = [];
+    index = 0
+    for output in test_output:
+      input = test_input.iloc[index, :]
+      test_data.append(tuple([torch.Tensor(input.values), output]))
+      index +=1
+    return test_data
 
--- training/mom_training.py
+++ training/mom_training.py
@@ -1,17 +1,18 @@
 import sys
-sys.path.append(r'../')
-from stock_deeplearning.training.data_loader import stock_loader
+import os
+sys.path.append(r'../../')
+from stock_deeplearning.training.data_loader import StockLoader as SL
 
 import torch
-import torchvision
-import torchvision.transforms as transforms
+# import torchvision
+# import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
 class Para:
   def __init__(self):
-    self.first_num = 16
+    self.first_num = 21
     self.second_num = 8
     self.third_num = 4
     self.forth_num = 2
@@ -23,7 +24,7 @@ class Net(nn.Module):
     self.fc1 = nn.Linear(para.first_num, para.second_num)
     self.fc2 = nn.Linear(para.second_num, para.third_num)
     self.fc3 = nn.Linear(para.third_num, para.forth_num)
-    self.fc4 = nn.Linear(para.third_num, 1)
+    self.fc4 = nn.Linear(para.forth_num, 1)
 
   def forward(self, x):
     x = F.relu(self.fc1(x))
@@ -34,10 +35,10 @@ class Net(nn.Module):
 
 def training():
 
-  criterion = nn.CrossEntropyLoss()
+  criterion = nn.SmoothL1Loss()
   optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-  trainloader = stock_loader_i.train_loader
+  trainloader = stock_loader.train_loader()
 
   for epoch in range(2):  # loop over the dataset multiple times
 
@@ -51,15 +52,15 @@ def training():
 
       # forward + backward + optimize
       output = net(train_inputs)
-      loss = criterion(output, train_output)
+      loss = criterion(output, torch.Tensor([train_output]))
       loss.backward()
       optimizer.step()
 
       # print statistics
       running_loss += loss.item()
-      if i % 2000 == 1999:    # print every 2000 mini-batches
+      if i % 20 == 19:    # print every 2000 mini-batches
         print('[%d, %5d] loss: %.3f' %
-              (epoch + 1, i + 1, running_loss / 2000))
+              (epoch + 1, i + 1, running_loss / 20))
         running_loss = 0.0
 
   print('Finished Training')
@@ -69,19 +70,20 @@ def training():
 def inferencing():
   correct = 0
   total = 0
+  testloader = stock_loader.test_loader()
   with torch.no_grad():
     for data in testloader:
       test_inputs, test_output = data
       output = net(test_inputs)
       total += 1
-      correct += (test_output - output) / test_output
+      correct += (test_output - output) / output
+      print("total", total, "correct", correct)
 
   print('Accuracy of the network on the ', total, 'test inputs: %d %%' % (
       100 * correct / total))
 
-PATH = '../../../data/training/momentum_net.pth'
-stock_loader_i = stock_loader(stock_code="000001.XSHE", path="../../../data",
-                              t_vs_t=0.1, train_day="5day")
+PATH = '../../data/momentum_net.pth'
+stock_loader = SL("000001.XSHE", "../../data/talib_factor", 0.9, "5day")
 net = Net()
 training()
 inferencing()
