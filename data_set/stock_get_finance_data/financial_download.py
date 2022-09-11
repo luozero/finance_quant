8fd838ed89d5239292005c93e7033ad51fa2987f
luozero <luozero@126.com>
2022-09-11 17:51:02 +0800

    add counter to skip the data

--- data_set/stock_get_finance_data/financial_download.py
+++ data_set/stock_get_finance_data/financial_download.py
@@ -19,45 +19,46 @@ def Schedule(a,b,c):
         per = 100
     print('%.2f%%' % per)
     
-def try_download_csv(url,local,Schedule,stock):
+def try_download_csv(url,local,Schedule,stock, download_count):
   try:
     request.urlretrieve(url,local,Schedule)
     continue_download_this_stock = 0
   except :
-    print('fail to download stock ',stock, 'one times, and try it again!!!');
+    print('fail to download stock ',stock, 'download count ', download_count, 'one times, and try it again!!!');
+    print(url)
     continue_download_this_stock = 1
   return continue_download_this_stock
 
-def fetch_stock_finance_data(path,stock):
+def fetch_stock_finance_data(path,stock, download_count):
   #main finance
   url = MAIN_FINANCE.format(stock)
   local = os.path.join(path,'{}_main.csv'.format(stock))
-  continue_download_this_stock = try_download_csv(url,local,Schedule,stock)
+  continue_download_this_stock = try_download_csv(url,local,Schedule,stock, download_count)
   if continue_download_this_stock==1:
     return continue_download_this_stock
   
   #abstract
   url = ADBSTRACT_FINANCE.format(stock)
   local = os.path.join(path,'{}_abstract.csv'.format(stock))
-  continue_download_this_stock = try_download_csv(url,local,Schedule,stock)
+  continue_download_this_stock = try_download_csv(url,local,Schedule,stock, download_count)
   if continue_download_this_stock==1:
     return continue_download_this_stock
   #profit
   url = PROFIT_FINANCE.format(stock)
   local = os.path.join(path,'{}_profit.csv'.format(stock))
-  continue_download_this_stock = try_download_csv(url,local,Schedule,stock)
+  continue_download_this_stock = try_download_csv(url,local,Schedule,stock, download_count)
   if continue_download_this_stock==1:
     return continue_download_this_stock
   #cash
   url = CASH_FINANCE.format(stock)
   local = os.path.join(path,'{}_cash.csv'.format(stock))
-  continue_download_this_stock = try_download_csv(url,local,Schedule,stock)
+  continue_download_this_stock = try_download_csv(url,local,Schedule,stock, download_count)
   if continue_download_this_stock==1:
     return continue_download_this_stock
   #loans
   url = LOANS_FINANCE.format(stock)
   local = os.path.join(path,'{}_loans.csv'.format(stock))
-  continue_download_this_stock = try_download_csv(url,local,Schedule,stock)
+  continue_download_this_stock = try_download_csv(url,local,Schedule,stock, download_count)
   if continue_download_this_stock==1:
     return continue_download_this_stock
 
@@ -96,15 +97,22 @@ def download_finance(path_root = '../../../data/'):
   stock_index = dr.read_index()
   stock_codes = stock_codes[stock_index:]
 
+  DOWNLOAD_THR = 100
   for stock in (stock_codes):
     print("fetching stock", stock)
     continue_download_this_stock = 1
+    download_count = 0
     while continue_download_this_stock == 1:
-      continue_download_this_stock = fetch_stock_finance_data(path, stock)
-    # store_stock_index(download_stock_file, stock_index)
-    dr.write_index(stock_index)
-    stock_index = stock_index + 1
-    print("fetched stock", stock)
+      continue_download_this_stock = fetch_stock_finance_data(path, stock, download_count)
+      download_count = download_count + 1
+      if download_count > DOWNLOAD_THR:
+        break
+    
+    if download_count < DOWNLOAD_THR:
+      # store_stock_index(download_stock_file, stock_index)
+      dr.write_index(stock_index)
+      stock_index = stock_index + 1
+      print("download finance data successfully stock", stock)
 
 if __name__ == '__main__':
   download_finance()
