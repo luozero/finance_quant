143d811f202fdb3b1f40776919bb5ffda382d804
luozero <luozero@126.com>
2022-09-23 21:33:38 +0800

    update trade data

--- data_set/trade_data/process_daily_trade_data.py
+++ data_set/trade_data/process_daily_trade_data.py
@@ -47,13 +47,11 @@ class process_daily_trade_data(object):
       else:
 
         file_name = FILE_DAILY_TRADE.format(stock_code)
-
         dates = data_main[finance_main].columns[1:self.FLD.min_column]
 
         daily_trade_data = self.FLD.load_financical_data([file_name])[file_name]
 
         # no trade data skip this stock 
-        # print(daily_trade_data)
         if (daily_trade_data.shape[0] == 0):
           print("no this stock", stock_code, "data")
           self.DR.write_skip_stock(stock_code)
@@ -61,15 +59,10 @@ class process_daily_trade_data(object):
 
         daily_trade_dates = daily_trade_data.loc[:,'日期']
         trade_data_quarter = pd.DataFrame(columns=daily_trade_data.columns)
-        # print('trade_data_quarter ', daily_trade_data.columns)
-        times = 0
+
         for get_date in dates:
-          times = times + 1
-          # print(file_name, 'date is', get_date, 'fetch time', times)
 
           date_in_daily_trade_dates = nearest_date(daily_trade_dates, get_date)
-          # print(daily_trade_data[date_in_daily_trade_dates == daily_trade_dates])
-          # trade_data_quarter = trade_data_quarter.append(daily_trade_data[date_in_daily_trade_dates == daily_trade_dates])
           trade_data_quarter = pd.concat([trade_data_quarter, daily_trade_data[date_in_daily_trade_dates == daily_trade_dates]])
 
         # trade_data_quarter.index = dates
@@ -79,4 +72,3 @@ class process_daily_trade_data(object):
 
         self.DR.write_data(KEY_PROCESS, KEY_PROCESS_DAILY_TRADE_QUARTER_INDEX, self.proc_id)
         self.proc_id = self.proc_id + 1
-        print('this stock',stock_code, 'successfully downloaded')
